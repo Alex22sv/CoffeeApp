21657b5bc4470cd95d98ba2632a1b50e1cf12246
Alex22sv <00024123@uca.edu.sv>
2024-07-27 22:43:51 -0600

    Update database info in main menu

--- CoffeeApp/src/main/java/com/alex22sv/coffeeapp/MainMenuController.java
+++ CoffeeApp/src/main/java/com/alex22sv/coffeeapp/MainMenuController.java
@@ -1,4 +1,29 @@
 package com.alex22sv.coffeeapp;
 
+import com.alex22sv.coffeeapp.Classes.Config;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 public class MainMenuController extends Controller {
+    // Database info
+    @FXML
+    private Label databaseUsername;
+    @FXML
+    private Label databaseServer;
+    @FXML
+    private Label databaseName;
+    // App Version
+    @FXML
+    private Label appVersion;
+    // Initialize
+    @FXML
+    private void initialize(){
+        // Database info
+        databaseUsername.setText("User: " + Config.USERNAME.value);
+        databaseServer.setText("Server: " + Config.SERVER.value);
+        databaseName.setText("Database: " + Config.DATABASE.value);
+        // App version
+        appVersion.setText(Config.APP_VERSION.value);
+    }
 }
